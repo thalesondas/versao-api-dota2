5975ab9a8e8da4bc9d6efe686ac8a5119b20cee4
Thales <thalesondas0@gmail.com>
2024-09-27 18:22:17 -0300

    Mudando o título no browser e o favicon

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Footer from './components/Footer.js';
 import Header from './components/Header.js';
 import Main from './components/Main.js';
@@ -6,6 +7,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 const App = () => {
+
+  useEffect(() => {
+    document.title = 'Dota 2 API';
+  }, []);
+
   return (
     <div className="App">
       <Header />
