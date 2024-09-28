6a1c6db23261609e9cf342e4d914e57c97dbdc6c
Thales <thalesondas0@gmail.com>
2024-09-28 11:25:34 -0300

    Mudando a fonte do título no site

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
                         <Image style={{ float: 'left' }} src='https://cdn-icons-png.flaticon.com/512/588/588267.png' width="100px" height="100px" alt='Dota 2 logo'></Image>
                     </Col>
                     <Col>
-                        <p>DOTA 2 API</p>
+                        <h1>DOTA 2 API</h1>
                     </Col>
                     <Col>
                         <div></div>
